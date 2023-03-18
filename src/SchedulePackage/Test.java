749ff13a450eae575e0054340f6ccbf34f46d5d1
ammar813 <128160249+ammar813@users.noreply.github.com>
2023-03-18 14:15:05 +0300

    Add files via upload

---
+++ src/SchedulePackage/Test.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package SchedulePackage;
+
+/**
+ *
+ * @author ammar
+ */
+public class Test {
+
+   static int a = 716;
+    public static void main(String[] args) {
+       
+         int digit = (int) ((a / 100) % 10);
+         System.out.print(digit + " ");
+         // now divide value by 10 to position for next digit.
+      
+    }
+    
+}
